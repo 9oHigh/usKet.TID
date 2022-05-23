98dfe58a4946050dcb624533244c0312d8126c33
9oHigh <wkdska957@naver.com>
2022-05-23 23:25:31 +0900

    Update xcschemes

---
+++ usket_TID/usket_TIDTests/usket_TIDTests.swift
@@ -0,0 +1,32 @@
+//
+//  usket_TIDTests.swift
+//  usket_TIDTests
+//
+//  Created by 이경후 on 2022/05/23.
+//
+
+import XCTest
+
+class usket_TIDTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
